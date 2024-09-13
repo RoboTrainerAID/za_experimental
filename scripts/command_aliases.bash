c8f466c89a348087d91f21571597574e283b9c8a
Andreas Zachariae <andreas.zachariae@gmail.com>
2024-09-13 16:29:21 +0200

    Update shortcuts and rviz, rqt settings

---
+++ scripts/command_aliases.bash
@@ -0,0 +1,3 @@
+alias ws="cd /home/robotrainer/workspace/ros_ws_melodic_robotrainer/src/"
+alias rt2_init="rosservice call /base/driver/init"
+alias rt2_recover="rosservice call /base/driver/recover"
