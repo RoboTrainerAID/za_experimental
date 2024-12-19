66283e861959ddd7a1526fd00f436a50e22136ab
AndreasZachariae <andreas.zachariae@gmail.com>
2024-12-19 16:32:57 +0100

    Add script for git fetch all

---
+++ scripts/git_fetch_all.bash
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# change in ros workspace
+cd /home/robotrainer/workspace/ros_ws_melodic_robotrainer/src
+
+# Iterate through all items in the current directory
+for dir in */; do
+    # Check if the item is a directory
+    if [ -d "$dir" ]; then
+        # Enter the directory
+        cd "$dir" || continue
+
+        # Check if it is a Git repository
+        if [ -d ".git" ]; then
+
+            # Check if the specific remote exists
+            if git remote | grep -q "robotrainer_github"; then
+                echo "Fetching from 'robotrainer_github' in $dir"
+                git fetch robotrainer_github
+            else
+                echo "Remote 'robotrainer_github' not found in $dir. Skipping."
+            fi
+        else
+            echo "$dir is not a Git repository. Skipping."
+        fi
+
+        # Return to the parent directory
+        cd ..
+    fi
+
+done
