15cbc1ac4710349d6e3d12e1ab6c987ec42a232b
AndreasZachariae <andreas.zachariae@gmail.com>
2024-12-19 12:23:49 +0100

    Fix git user script

--- scripts/git_change_user.bash
+++ scripts/git_change_user.bash
@@ -1,10 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
 # User to change to, default is AndreasZachariae
 GITHUB_USER=${1:-AndreasZachariae}
 
+# This has to started with 'source' to change the environment of the current shell
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    echo "This script must be sourced: use 'source $0'" >&2
+    exit 1
+fi
+
 # Add the new user's ssh key to the ssh-agent
-eval "$(ssh-agent -s)"
+if [ -z "$SSH_AUTH_SOCK" ]; then
+    eval "$(ssh-agent -s)" > /dev/null
+fi
 ssh-add -D
 ssh-add ~/.ssh/${GITHUB_USER}
 GITHUB_EMAIL=$(ssh-add -L | awk '{print $NF}')
